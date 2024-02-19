0e23cb44ee66ca559f7f49edc8fce2509c78f452
cyl19970726 <15258378443@163.com>
2024-02-19 17:02:07 +0800

    update waitutilUXTO to support minvalue

--- src/blockstream_utils.ts
+++ src/blockstream_utils.ts
@@ -1,43 +1,55 @@
 import axios, { AxiosResponse } from "axios";
 
 const blockstream = new axios.Axios({
-    baseURL: `https://blockstream.info/testnet/api`
+  baseURL: `https://blockstream.info/testnet/api`,
 });
 
-export async function waitUntilUTXO(address: string) {
-    return new Promise<IUTXO[]>((resolve, reject) => {
-        let intervalId: any;
-        const checkForUtxo = async () => {
-            try {
-                const response: AxiosResponse<string> = await blockstream.get(`/address/${address}/utxo`);
-                const data: IUTXO[] = response.data ? JSON.parse(response.data) : undefined;
-                console.log(data);
-                if (data.length > 0) {
-                    resolve(data);
-                    clearInterval(intervalId);
-                }
-            } catch (error) {
-                reject(error);
-                clearInterval(intervalId);
+export async function waitUntilUTXO(address: string, minValue?: number) {
+  return new Promise<IUTXO[]>((resolve, reject) => {
+    let intervalId: any;
+    const checkForUtxo = async () => {
+      try {
+        const response: AxiosResponse<string> = await blockstream.get(
+          `/address/${address}/utxo`,
+        );
+        const data: IUTXO[] = response.data
+          ? JSON.parse(response.data)
+          : undefined;
+        console.log(`unspent utxos:`, data);
+        if (data.length > 0) {
+          if (minValue !== undefined) {
+            const utxo = data.find((utxo) => utxo.value >= minValue);
+            if (utxo) {
+              resolve([utxo]);
+              clearInterval(intervalId);
             }
-        };
-        intervalId = setInterval(checkForUtxo, 10000);
-    });
+          } else {
+            resolve(data);
+            clearInterval(intervalId);
+          }
+        }
+      } catch (error) {
+        reject(error);
+        clearInterval(intervalId);
+      }
+    };
+    intervalId = setInterval(checkForUtxo, 10000);
+  });
 }
 
 export async function broadcast(txHex: string) {
-    const response: AxiosResponse<string> = await blockstream.post('/tx', txHex);
-    return response.data;
+  const response: AxiosResponse<string> = await blockstream.post("/tx", txHex);
+  return response.data;
 }
 
 interface IUTXO {
-    txid: string;
-    vout: number;
-    status: {
-        confirmed: boolean;
-        block_height: number;
-        block_hash: string;
-        block_time: number;
-    };
-    value: number;
+  txid: string;
+  vout: number;
+  status: {
+    confirmed: boolean;
+    block_height: number;
+    block_hash: string;
+    block_time: number;
+  };
+  value: number;
 }
